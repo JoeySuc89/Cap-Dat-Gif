2f95a007210b741a39f33316e5679471bd7a0953
unknown <evan2580@gmail.com>
2019-06-06 21:18:19 -0400

    updated sequelize db

---
+++ migrations/20190607005033-create-memes.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('memes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.INTEGER
+      },
+      dislikes: {
+        type: Sequelize.INTEGER
+      },
+      memeText: {
+        type: Sequelize.STRING
+      },
+      commentText: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('memes');
+  }
+};
